7fe0f6c03d3cd50738726d1016dec18f39a4bac4
SidakSingh <100962515+SidakSingh@users.noreply.github.com>
2025-12-03 23:55:17 -0500

    Fixed enqueue dependency array

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 function twentytwentyfive_child_enqueue_styles()
 {
+
     $parent_style = 'parent-style';
 
     wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
